4180e1b604378b25891aedc4fa6badac51d2bae8
KainWong <877841528@qq.com>
2019-10-22 14:18:17 +0800

    other pages

--- src/pages/level2/index.vue
+++ src/pages/level2/index.vue
@@ -1,10 +1,99 @@
 <template>
-<div>
-    <div class="question">{{question}}</div>
-    <div class="choose-item" v-for="(item,index) in chooseList" :key="index">{{item}}</div>
+<div class="main">
+    <div class="mini-main">
+        <div class="choose-title">{{question}}</div>
+        <div class="choose-type" v-for="(item,index) in chooseList" :key="index" @click="nextLevel">{{item}}</div>
+        <div class="dialog" v-if="showWarn">
+        <div class="message">{{warnMessage}}</div>
+        </div>
+    </div>
 </div>
 </template>
-<style>
+<style lang="wxss">
+page{
+  height: 100% !important;
+}
+.main {
+  height: 100%;
+  background-size: 100% 100%;
+  position: relative;
+}
+
+.main:before {
+    background: url('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1571723003978&di=f21df3fd97f2bbf67bf8e893524ee092&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201703%2F08%2F20170308124304_eNkzh.jpeg');
+    background-size: 100% 100%;
+    width: 100%;   
+    height: 100%;          
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -3;
+    -webkit-filter:blur(2px);
+    filter: blur(2px);
+}
+
+.poster {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  z-index: -1
+}
+
+.main .mini-main {
+  height: 100%;
+  padding: 0 20rpx;
+  position: absolute;
+  overflow-y: scroll;
+}
+
+.dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: disappear 2s
+}
+
+.dialog .message {
+  padding:15rpx 20rpx;
+  color: white;
+  background: rgba(0,0,0,0.5);
+}
+
+.choose-title {
+  font-size: 55rpx;
+  color:orange;
+  font-family: LiSu;
+  margin: 20rpx 0;
+  margin-top: 200rpx;
+  margin-bottom: 40rpx;
+  text-align: center;
+}
+
+.choose-type {
+  font-size: 40rpx;
+  color:orangered;
+  font-family: LiSu;
+  margin: 20rpx 0;
+  text-align: center;
+}
+
+@keyframes disappear {
+  0%{
+    opacity: 1
+  }
+  50%{
+    opacity: 1
+  }
+  100% {
+    opacity: 0;
+  }
+}
 </style>
 <script>
     export default {
@@ -14,11 +103,11 @@
         props: {},
         data() {
             return {
-                question: '',
+                question: '看到一个小孩在旁边嚎啕大哭并惊声尖叫，你会什么反应？',
                 chooseList: [
-                    'A.',
-                    'B.',
-                    'C.'
+                    'A. 给他一拳',
+                    'B. 一拳解决不了问题的话，再补上几拳',
+                    'C. 这小孩太吵了，给多少拳都难以抚平我的烦躁'
                 ]
             }
         },
@@ -27,6 +116,20 @@
         watch: {
         },
         methods: {
+            showWarnDialog(text) {
+                this.warnMessage = text;
+                this.showWarn = true;
+                setTimeout(()=> {
+                    this.showWarn = false
+                },1500)
+            },
+            nextLevel() {
+                this.showWarnDialog('没有最暴力，只有更暴力~');
+                const url = '../level3/main';
+                setTimeout(()=> {
+                    wx.navigateTo({ url })
+                },2000)
+            }
         },
         created() {
         },
---
+++ src/pages/level3/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="main">
+    <div class="mini-main">
+        <div class="choose-title">{{question}}</div>
+        <div class="choose-type" v-for="(item,index) in chooseList" :key="index" @click="nextLevel">{{item}}</div>
+    </div>
+    <div class="message">{{warnMessage}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+        question: '你看到一个路人不小心滑倒摔了个屁股蹲儿，你会怎么做',
+        chooseList: [
+            'A. 赶忙跑上前去，询问情况',
+            'B. 抑制住内心的喜悦，违心的发出怜悯“Oh,poor guy”',
+            'C. 当场失控，笑得前仰后翻'
+        ]
+    }
+  },
+
+  watch: {
+  },
+  computed: {
+  },
+  create() {
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style lang='wxss'>
+page{
+  height: 100% !important;
+}
+.main {
+  height: 100%;
+  background-size: 100% 100%;
+  position: relative;
+}
+
+.main:before {
+    background: url('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1571563837508&di=1828c37922e0d25f6badcbf07b46b27a&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201510%2F05%2F20151005053743_hcFTr.thumb.700_0.jpeg');
+    background-size: 100% 100%;
+    width: 100%;   
+    height: 100%;          
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -3;
+    -webkit-filter:blur(2px);
+    filter: blur(2px);
+}
+
+.poster {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  z-index: -1
+}
+
+.main .mini-main {
+  height: 100%;
+  padding: 0 20rpx;
+  position: absolute;
+  overflow-y: scroll;
+}
+
+.dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: disappear 2s
+}
+
+.dialog .message {
+  padding:15rpx 20rpx;
+  color: white;
+  background: rgba(0,0,0,0.5);
+}
+
+.choose-title {
+  font-size: 55rpx;
+  color:orange;
+  font-family: LiSu;
+  margin: 20rpx 0;
+  margin-top: 200rpx;
+  margin-bottom: 40rpx;
+  text-align: center;
+}
+
+.choose-type {
+  font-size: 40rpx;
+  color:orangered;
+  font-family: LiSu;
+  margin: 20rpx 0;
+  text-align: center;
+}
+
+@keyframes disappear {
+  0%{
+    opacity: 1
+  }
+  50%{
+    opacity: 1
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
